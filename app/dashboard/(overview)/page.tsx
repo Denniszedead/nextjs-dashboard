a8983ec43914ec6997a07e4319f86501e90c3a71
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2025-03-02 23:31:47 +0800

    Replace 'Dashboard' with 'Home'

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,7 +7,7 @@ import {Suspense} from "react";
 import {Metadata} from "next";
 
 export const metadata: Metadata = {
-    title: 'Dashboard',
+    title: 'Home',
 };
 
 export default async function Page() {
